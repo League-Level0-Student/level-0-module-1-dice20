99a8579c84867d5ad39ef38e907e27c13f1d9109
i-lab user <i-labuser@SDPL88536-iMacProLO.local>
2019-10-15 17:54:05 -0700

    unbirthday

---
+++ src/unbday.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class unbday {
+public static void main(String[] args) {
+	String bday =JOptionPane.showInputDialog("what is your birthday");
+	
+	if("10/15".equals(bday)) {
+		
+		JOptionPane.showMessageDialog(null,"you`re old");
+	}
+	
+	
+
+
+JOptionPane.showMessageDialog(null,"you`re old");
+
+}
+}
