8fa0e90ae20cc3a92ee9bd6449eed2055eec6505
i-lab user <i-labuser@SDPL88533-iMacProLO.local>
2019-10-29 17:55:59 -0700

    secret messaege box

---
+++ src/remakable.java
@@ -0,0 +1,61 @@
+import javax.swing.JOptionPane;
+
+public class remakable {
+	public static void main(String[] args) {
+		String awesome = JOptionPane.showInputDialog("tell me yor name");
+		if("Michiyo".equals(awesome)) {
+			JOptionPane.showMessageDialog(null,awesome + " is the best coding teacher");
+		}	
+		
+		else{
+			JOptionPane.showMessageDialog(null, awesome + " is remarkable" );			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
