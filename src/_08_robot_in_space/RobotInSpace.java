13a752582f4dd22f6a7fc0b2183873453969286f
i-lab user <i-labuser@SDPL88532-iMacProLO.local>
2020-02-11 17:55:05 -0800

    zombie eyes

--- src/_08_robot_in_space/RobotInSpace.java
+++ src/_08_robot_in_space/RobotInSpace.java
@@ -34,12 +34,24 @@ if(keyPressed==38) {
 rob.setAngle(0);	
 rob.microMove(1);}
 		// 4. If the down arrow is pressed, move the Robot down.
-
+if(keyPressed==40) {
+rob.setAngle(180);
+rob.microMove(1);	
+	
+}
 		// 5. If the left arrow is pressed, make the Robot go left.
-
+if(keyPressed==37) {
+rob.setAngle(270);
+rob.microMove(1);	
+	
+}
 		// 6. If right is pressed, move the Robot right.
-
-		// 7. Run your program and move the Robot to RD-2D for a surprise!
+if(keyPressed==39) {
+rob.setAngle(90);
+rob.microMove(1);	
+	
+}
+		// 7. Run your program and move the Robot to R2-D2 for a surprise!
 	}
 
 	private void checkIfR2D2Found() throws Exception {
---
+++ src/_09_zombie_eyes/zombie_eyes/mariopic/mariopic.pde
@@ -0,0 +1,18 @@
+   
+    void setup(){
+        size(1200,1200);
+        
+       
+        
+        
+        PImage face = loadImage("mario (1).png");
+image(face, 0,0);
+        face.resize(width, height);
+         background(face);
+        
+        
+        
+    }
+
+    void draw(){
+    }
