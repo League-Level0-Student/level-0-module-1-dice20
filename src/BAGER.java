219ff157a0ff46a35614614d231d6b66a5e539fb
i-lab user <i-labuser@SDPL88537-iMacProLO.local>
2020-02-25 17:57:16 -0800

    bager

---
+++ src/BAGER.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class BAGER {
+public static void main(String[] args) {
+	for (int i = 0; i < 100; i++) {
+	System.out.println("bager");	
+	}
+}
+}
---
+++ src/y33333333333333333333333333333333333333337.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class y33333333333333333333333333333333333333337 {
+	public static void main(String[] args) {
+		int age=0;
+		for (int i = 0; i < 10; i++) {
+			JOptionPane.showMessageDialog( null, "alive");	
+		}
+	}
+}
